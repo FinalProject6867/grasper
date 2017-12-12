217773aa5c81615fabb4124af845df0475c6d181
Rachel Holladay <rachel.holladay@gmail.com>
2017-12-12 09:08:03 -0500

    New fig making

--- figGen/basicPlots.py
+++ figGen/basicPlots.py
@@ -5,9 +5,12 @@ import numpy, csv
 import matplotlib.pyplot as plt
 from ss_plotting.make_plots import plot
 
+networks = ['andreas', 'gqcnn', 'inception', 'res_net']
+networkName = networks[3]
+
 def readCSV(filename):
     dataStorage = []
-    with open('data/inception_net/{}.csv'.format(filename), 'rb') as csvfile:
+    with open('data/recent_results/{}_splits/{}.csv'.format(networkName, filename), 'rb') as csvfile:
         datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in datareader:
             pieces = row[0].split(',')
@@ -22,23 +25,25 @@ def plotAccuracy(train, val):
           [val[:, 1], val[:, 2]]],
           series_colors=['green', 'red'],
           series_labels=['Training', 'Validation'],
+          plot_ylim=[0, 0.8],
           show_plot=False,
           plot_xlabel='Number of Steps',
           plot_ylabel='Accuracy',
           #savefile_size=(2.5, 1.5),
-          plot_title='Accuracy using Inception Net',
-          savefile='figs/inception_accuracy.pdf')
+          #plot_title='Accuracy using Inception Net',
+          savefile='figs/{}_accuracy.pdf'.format(networkName))
 
 def plotLoss(train, val):
     plot([[train[:, 1], train[:, 2]], 
           [val[:, 1], val[:, 2]]], 
           series_colors=['green', 'red'],
           series_labels=['Training', 'Validation'], 
+          plot_ylim=[0, 9],
           show_plot=False,
           plot_xlabel='Number of Steps', 
           plot_ylabel='Loss',
-          plot_title='Loss using Inception Net',
-          savefile='figs/inception_loss.pdf')
+          #plot_title='Loss using Inception Net',
+          savefile='figs/{}_loss.pdf'.format(networkName))
 
 if __name__ == '__main__':
     train_acc = readCSV('train_acc')
